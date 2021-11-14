3cb5fa9e2ad37bdb6a91f9ef16fe57f126a94356
kiril <karkarkarkirill@mail.ru>
2021-11-14 17:58:33 +0700

    Произведены мелкие изменения интерфейса

--- DiceUI/DiceUI/MainForm.Designer.cs
+++ DiceUI/DiceUI/MainForm.Designer.cs
@@ -51,6 +51,7 @@ namespace DiceUI
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -60,12 +61,13 @@ namespace DiceUI
             // 
             // splitContainer1
             // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
             this.splitContainer1.Panel1.Controls.Add(this.label6);
             this.splitContainer1.Panel1.Controls.Add(this.label5);
             this.splitContainer1.Panel1.Controls.Add(this.label4);
@@ -87,17 +89,19 @@ namespace DiceUI
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
-            this.splitContainer1.Size = new System.Drawing.Size(943, 541);
-            this.splitContainer1.SplitterDistance = 306;
+            this.splitContainer1.Size = new System.Drawing.Size(852, 346);
+            this.splitContainer1.SplitterDistance = 269;
+            this.splitContainer1.SplitterWidth = 3;
             this.splitContainer1.TabIndex = 0;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label6.Location = new System.Drawing.Point(13, 282);
+            this.label6.Location = new System.Drawing.Point(10, 210);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(85, 17);
+            this.label6.Size = new System.Drawing.Size(63, 13);
             this.label6.TabIndex = 17;
             this.label6.Text = "(от 3 до 24)";
             // 
@@ -105,9 +109,10 @@ namespace DiceUI
             // 
             this.label5.AutoSize = true;
             this.label5.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label5.Location = new System.Drawing.Point(13, 236);
+            this.label5.Location = new System.Drawing.Point(10, 172);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(85, 17);
+            this.label5.Size = new System.Drawing.Size(63, 13);
             this.label5.TabIndex = 16;
             this.label5.Text = "(от 8 до 15)";
             // 
@@ -115,9 +120,10 @@ namespace DiceUI
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label4.Location = new System.Drawing.Point(12, 183);
+            this.label4.Location = new System.Drawing.Point(9, 129);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(93, 17);
+            this.label4.Size = new System.Drawing.Size(69, 13);
             this.label4.TabIndex = 15;
             this.label4.Text = "(от 10 до 30)";
             // 
@@ -125,9 +131,10 @@ namespace DiceUI
             // 
             this.label3.AutoSize = true;
             this.label3.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label3.Location = new System.Drawing.Point(12, 136);
+            this.label3.Location = new System.Drawing.Point(9, 91);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 17);
+            this.label3.Size = new System.Drawing.Size(69, 13);
             this.label3.TabIndex = 14;
             this.label3.Text = "(от 30 до 60)";
             // 
@@ -135,62 +142,69 @@ namespace DiceUI
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Location = new System.Drawing.Point(9, 52);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.Size = new System.Drawing.Size(75, 13);
             this.label2.TabIndex = 13;
             this.label2.Text = "(от 60 до 120)";
             // 
             // BuildObjectbutton
             // 
-            this.BuildObjectbutton.Location = new System.Drawing.Point(15, 500);
+            this.BuildObjectbutton.Location = new System.Drawing.Point(11, 233);
+            this.BuildObjectbutton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.BuildObjectbutton.Name = "BuildObjectbutton";
-            this.BuildObjectbutton.Size = new System.Drawing.Size(163, 29);
+            this.BuildObjectbutton.Size = new System.Drawing.Size(123, 37);
             this.BuildObjectbutton.TabIndex = 12;
             this.BuildObjectbutton.Text = "Построить объект";
             this.BuildObjectbutton.UseVisualStyleBackColor = true;
             // 
             // EdgeWidth
             // 
-            this.EdgeWidth.Location = new System.Drawing.Point(162, 265);
+            this.EdgeWidth.Location = new System.Drawing.Point(122, 196);
+            this.EdgeWidth.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.EdgeWidth.Name = "EdgeWidth";
-            this.EdgeWidth.Size = new System.Drawing.Size(141, 22);
+            this.EdgeWidth.Size = new System.Drawing.Size(139, 20);
             this.EdgeWidth.TabIndex = 10;
             this.EdgeWidth.TextChanged += new System.EventHandler(this.EdgeWidth_TextChanged);
             this.EdgeWidth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
             // 
             // DredgingDiametr
             // 
-            this.DredgingDiametr.Location = new System.Drawing.Point(162, 212);
+            this.DredgingDiametr.Location = new System.Drawing.Point(122, 153);
+            this.DredgingDiametr.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DredgingDiametr.Name = "DredgingDiametr";
-            this.DredgingDiametr.Size = new System.Drawing.Size(141, 22);
+            this.DredgingDiametr.Size = new System.Drawing.Size(139, 20);
             this.DredgingDiametr.TabIndex = 9;
             this.DredgingDiametr.TextChanged += new System.EventHandler(this.DredgingDiametr_TextChanged);
             this.DredgingDiametr.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
             // 
             // DiceThickness
             // 
-            this.DiceThickness.Location = new System.Drawing.Point(162, 163);
+            this.DiceThickness.Location = new System.Drawing.Point(122, 113);
+            this.DiceThickness.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DiceThickness.Name = "DiceThickness";
-            this.DiceThickness.Size = new System.Drawing.Size(141, 22);
+            this.DiceThickness.Size = new System.Drawing.Size(139, 20);
             this.DiceThickness.TabIndex = 8;
             this.DiceThickness.TextChanged += new System.EventHandler(this.DiceThickness_TextChanged);
             this.DiceThickness.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
             // 
             // DiceWidth
             // 
-            this.DiceWidth.Location = new System.Drawing.Point(162, 116);
+            this.DiceWidth.Location = new System.Drawing.Point(122, 75);
+            this.DiceWidth.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DiceWidth.Name = "DiceWidth";
-            this.DiceWidth.Size = new System.Drawing.Size(141, 22);
+            this.DiceWidth.Size = new System.Drawing.Size(139, 20);
             this.DiceWidth.TabIndex = 7;
             this.DiceWidth.TextChanged += new System.EventHandler(this.DiceWidth_TextChanged);
             this.DiceWidth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
             // 
             // DiceHeight
             // 
-            this.DiceHeight.Location = new System.Drawing.Point(162, 68);
+            this.DiceHeight.Location = new System.Drawing.Point(122, 36);
+            this.DiceHeight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DiceHeight.Name = "DiceHeight";
-            this.DiceHeight.Size = new System.Drawing.Size(141, 22);
+            this.DiceHeight.Size = new System.Drawing.Size(139, 20);
             this.DiceHeight.TabIndex = 0;
             this.DiceHeight.TextChanged += new System.EventHandler(this.DiceHeight_TextChanged);
             this.DiceHeight.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
@@ -198,72 +212,89 @@ namespace DiceUI
             // EdgeWidthlabel
             // 
             this.EdgeWidthlabel.AutoSize = true;
-            this.EdgeWidthlabel.Location = new System.Drawing.Point(12, 265);
+            this.EdgeWidthlabel.Location = new System.Drawing.Point(9, 196);
+            this.EdgeWidthlabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.EdgeWidthlabel.Name = "EdgeWidthlabel";
-            this.EdgeWidthlabel.Size = new System.Drawing.Size(133, 17);
+            this.EdgeWidthlabel.Size = new System.Drawing.Size(103, 13);
             this.EdgeWidthlabel.TabIndex = 5;
             this.EdgeWidthlabel.Text = "Ширина каемки (E)";
             // 
             // DedgingDiametrlabel
             // 
             this.DedgingDiametrlabel.AutoSize = true;
-            this.DedgingDiametrlabel.Location = new System.Drawing.Point(12, 215);
+            this.DedgingDiametrlabel.Location = new System.Drawing.Point(9, 155);
+            this.DedgingDiametrlabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.DedgingDiametrlabel.Name = "DedgingDiametrlabel";
-            this.DedgingDiametrlabel.Size = new System.Drawing.Size(144, 17);
+            this.DedgingDiametrlabel.Size = new System.Drawing.Size(113, 13);
             this.DedgingDiametrlabel.TabIndex = 4;
             this.DedgingDiametrlabel.Text = "Диаметр выемки (D)";
             // 
             // DiceThicknesslabel
             // 
             this.DiceThicknesslabel.AutoSize = true;
-            this.DiceThicknesslabel.Location = new System.Drawing.Point(12, 166);
+            this.DiceThicknesslabel.Location = new System.Drawing.Point(9, 115);
+            this.DiceThicknesslabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.DiceThicknesslabel.Name = "DiceThicknesslabel";
-            this.DiceThicknesslabel.Size = new System.Drawing.Size(132, 17);
+            this.DiceThicknesslabel.Size = new System.Drawing.Size(101, 13);
             this.DiceThicknesslabel.TabIndex = 3;
             this.DiceThicknesslabel.Text = "Толщина кости (B)";
             // 
             // DiceWidthlabel
             // 
             this.DiceWidthlabel.AutoSize = true;
-            this.DiceWidthlabel.Location = new System.Drawing.Point(12, 119);
+            this.DiceWidthlabel.Location = new System.Drawing.Point(9, 77);
+            this.DiceWidthlabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.DiceWidthlabel.Name = "DiceWidthlabel";
-            this.DiceWidthlabel.Size = new System.Drawing.Size(123, 17);
+            this.DiceWidthlabel.Size = new System.Drawing.Size(94, 13);
             this.DiceWidthlabel.TabIndex = 2;
             this.DiceWidthlabel.Text = "Ширина кости (C)";
             // 
             // DiceHeightlabel
             // 
             this.DiceHeightlabel.AutoSize = true;
-            this.DiceHeightlabel.Location = new System.Drawing.Point(12, 71);
+            this.DiceHeightlabel.Location = new System.Drawing.Point(9, 38);
+            this.DiceHeightlabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.DiceHeightlabel.Name = "DiceHeightlabel";
-            this.DiceHeightlabel.Size = new System.Drawing.Size(121, 17);
+            this.DiceHeightlabel.Size = new System.Drawing.Size(93, 13);
             this.DiceHeightlabel.TabIndex = 1;
             this.DiceHeightlabel.Text = "Высота кости (A)";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(186, 17);
+            this.label1.Size = new System.Drawing.Size(145, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Параметры объекта (в мм)";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(11, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(7, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(583, 432);
+            this.pictureBox1.Size = new System.Drawing.Size(434, 326);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 37);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Построить объект по умолчанию";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(943, 541);
+            this.ClientSize = new System.Drawing.Size(724, 348);
             this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -299,5 +330,6 @@ namespace DiceUI
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button1;
     }
 }
