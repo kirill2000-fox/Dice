7d8c9e4369cfd9c7e94d28d461628daeb5fc7d65
kiril <karkarkarkirill@mail.ru>
2021-12-07 23:23:37 +0700

    Исправлены некоторые ошибки

--- DiceUI/DiceUI/DiceBuilder.cs
+++ DiceUI/DiceUI/DiceBuilder.cs
@@ -7,11 +7,11 @@ namespace DiceUI
     class DiceBuilder
     {
         private KompasObject _kompasObject;
-        private Parameters _diceParameters;
+        private NameParameters _diceParameters;
         public DiceBuilder(Parameters parameters, KompasObject kompas)
         {
             //Получение параметров 
-            _diceParameters = parameters;
+            _diceParameters = NameParameters;
             //Получение объекта KOMPAS 3D
             _kompasObject = kompas;
             //Создание объекта
@@ -26,13 +26,13 @@ namespace DiceUI
             ksDocument3D iDocument3D = (ksDocument3D)_kompasObject.Document3D();
             iDocument3D.Create(false, true);
             //Получение интерфейса детали
-            ksPart iPart = (ksPart)iDocument3D.GetPart((short)Part_Type.pTop_Part);
+            ksPart part = (ksPart)iDocument3D.GetPart((short)Part_Type.pTop_Part);
             //Создание прямоугольника
-            CreateRectangle(iPart);
+            CreateRectangle(part);
             //Создание каемки
-            CreateEdge(iPart);
+            CreateEdge(part);
             //Создание выемки
-            CreateDredging(iPart); 
+            CreateDredging(part); 
 
             /// <summary>
             /// Построение прямоугольника
@@ -40,11 +40,11 @@ namespace DiceUI
             /// <param name="iPart"></param>
             void CreateRectangle(ksPart iPart)
             {
-                if (_diceParameters.parameters(Parameters.DiceHeight).Value != 0 &&
-                    _diceParameters.Parameter(Parameters.DiceWidth).Value != 0)
+                if (_diceParameters.DiceHeight != 0 &&
+                    _diceParameters.DiceWidth != 0)
                 {
                     //Получение глубины выдавливания
-                    var height = _diceParameters.Parameter(Parameters.DiceThickness).Value;
+                    var height = _diceParameters.DiceThickness;
                     //Выдавливание
                     object iSketch = null;
                     ExtrusionOperation(iPart, iSketch, height, false);
@@ -58,11 +58,11 @@ namespace DiceUI
             /// <param name="iPart"></param>
             void CreateEdge(ksPart iPart)
             {
-                if (_diceParameters.parameters(Parameters.DiceHeight).Value != 0 &&
-                    _diceParameters.parameters(Parameters.DiceWidth).Value != 0)
+                if (_diceParameters.DiceHeight != 0 &&
+                    _diceParameters.DiceWidth != 0)
                 {
                     //Получение глубины выдавливания
-                    var height = _diceParameters.Parameter(Parameters.DiceThickness).Value;
+                    var height = _diceParameters.DiceThickness;
                     //Выдавливание
                     object iSketch = null;
                     ExtrusionOperation(iPart, iSketch, height, false);
@@ -75,11 +75,11 @@ namespace DiceUI
             /// </summary>
             void CreateDredging(ksPart iPart)
             {
-                if (_diceParameters.parameters(Parameters.DiceHeight).Value != 0 &&
-                    _diceParameters.Parameter(Parameters.DiceWidth).Value != 0)
+                if (_diceParameters.DiceHeight != 0 &&
+                    _diceParameters.DiceWidth != 0)
                 {
                     //Получение глубины выдавливания
-                    var height = _diceParameters.parameters(Parameters.DiceThickness).Value;
+                    var height = _diceParameters.DiceThickness;
                     //Выдавливание
                     object iSketch = null;
                     ExtrusionOperation(iPart, iSketch, height, false);
